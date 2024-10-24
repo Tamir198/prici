3b114caa6fd0414975631083a77f506b9a66c41a
Tamir Abutbul <1tamir198@gmail.com>
2024-10-24 18:06:05 +0300

    Installing types for fastify guard

--- libs/sdk/fastify/index.ts
+++ libs/sdk/fastify/index.ts
@@ -1,5 +1,4 @@
-// TODO check if need to download those types or remain with any
-// import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { FieldStateResult } from '@prici/shared-remult';
 import PriciSdk from '../index';
 
@@ -8,61 +7,65 @@ export interface FastifyGuardOptions {
   fieldId?: string;
   errorMessage?: string;
   incrementAmount?: number;
-  getAccountId?: (req?: any) => string | Promise<string>;
-  getFieldId?: (req?: any) => string | Promise<string>;
+  getAccountId?: (req?: FastifyRequest) => string | Promise<string>;
+  getFieldId?: (req?: FastifyRequest) => string | Promise<string>;
   getError?: (
-    req?: any,
+    req?: FastifyRequest,
     fieldStateResult?: FieldStateResult
   ) => string | Promise<string>;
-  getIncrementAmount?: (req?: any) => number;
+  getIncrementAmount?: (req?: FastifyRequest) => number;
 }
 
 export function fastifyGuard(
-  fastify: any,
+  fastify: FastifyInstance,
   opts: FastifyGuardOptions,
   done: () => void
 ) {
   const options = {
-    getAccountId: async (req: any) =>
+    getAccountId: async (req: FastifyRequest) =>
       req.headers['account-id'] || req.user?.account || req.user?.tenant,
-    getFieldId: async (req: any) => opts.fieldId || req.query['fieldId'],
-    getError: async (req?: any) =>
+    getFieldId: async (req: FastifyRequest) =>
+      opts.fieldId || req.query['fieldId'],
+    getError: async (req?: FastifyRequest) =>
       opts.errorMessage || opts.sdk.defaultErrorMessage,
     getIncrementAmount: () => opts.incrementAmount,
     ...opts,
   };
 
-  fastify.addHook('preHandler', async (req: any, reply: any) => {
-    const [accountId, fieldId] = await Promise.all([
-      options.getAccountId(req),
-      options.getFieldId(req),
-    ]);
+  fastify.addHook(
+    'preHandler',
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      const [accountId, fieldId] = await Promise.all([
+        options.getAccountId(req),
+        options.getFieldId(req),
+      ]);
 
-    if (!(accountId && fieldId)) {
-      // Proceed to the next handler if no accountId or fieldId is found
-      return;
-    }
-
-    const result = await options.sdk.getFieldState(accountId, fieldId);
-
-    if (!result.isAllowed) {
-      const errorMessage = await options.getError(req, result);
-      reply.status(402).send({ message: errorMessage });
-      return;
-    }
-
-    reply.raw.once('finish', () => {
-      if (reply.raw.statusCode.toString().startsWith('2')) {
-        options.sdk
-          .incrementField(
-            accountId,
-            fieldId,
-            options.getIncrementAmount(req) || undefined
-          )
-          .catch();
+      if (!(accountId && fieldId)) {
+        // Proceed to the next handler if no accountId or fieldId is found
+        return;
       }
-    });
-  });
+
+      const result = await options.sdk.getFieldState(accountId, fieldId);
+
+      if (!result.isAllowed) {
+        const errorMessage = await options.getError(req, result);
+        reply.status(402).send({ message: errorMessage });
+        return;
+      }
+
+      reply.raw.once('finish', () => {
+        if (reply.raw.statusCode.toString().startsWith('2')) {
+          options.sdk
+            .incrementField(
+              accountId,
+              fieldId,
+              options.getIncrementAmount(req) || undefined
+            )
+            .catch();
+        }
+      });
+    }
+  );
 
   done();
 }
